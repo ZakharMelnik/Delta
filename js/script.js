22f83047ac711c12328d5f8fcfd42102a8d87026
Zahar <melnik.zakhar222@gmail.com>
2020-08-31 22:04:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
 $(document).ready(function() {
 
+	$(".banner .arrow-down").click(function(){
+		$(".banner").css('background-image', 'url(img/main/banner2.jpg)');
+		$(".banner-descr").text('Контражурные логотипы для крупных компаний');
+		
+	});
+
+	$(".banner .arrow-up").click(function(){
+		$(".banner").css('background-image', 'url(img/main/banner1.jpg)');
+		$(".banner-descr").text('Комплексное оформление ТРЦ рекламными конструкциями');
+	});
+
 
 	$('.slider').slick({
 	  infinite: true,
 	  slidesToShow: 4,
 	  slidesToScroll: 4,
 	  arrows: false,
-	  dots: true
+	  dots: true,
+	  responsive: [{
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    }]
+	});
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
 	});
 
 	/*Header menu*/
